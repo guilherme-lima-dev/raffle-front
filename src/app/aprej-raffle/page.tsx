db708f3fb065ec54166152366312c468115d573a
Guilherme Felipe de Lima <guifellim20@gmail.com>
2024-09-11 13:40:20 -0300

    pages

--- src/app/aprej-raffle/page.tsx
+++ src/app/aprej-raffle/page.tsx
@@ -10,7 +10,7 @@ export default function OrderApprovalPage() {
 
     const handleApprove = async () => {
         try {
-            await axios.get(`http://ec2-44-201-203-12.compute-1.amazonaws.com/orders/${orderId}/approve`, {
+            await axios.get(`https://raffle.popingressos.com.br/orders/${orderId}/approve`, {
                 headers: {
                     accept: 'application/json',
                 },
@@ -25,7 +25,7 @@ export default function OrderApprovalPage() {
 
     const handleReject = async () => {
         try {
-             await axios.get(`http://ec2-44-201-203-12.compute-1.amazonaws.com/orders/${orderId}/reject`, {
+             await axios.get(`https://raffle.popingressos.com.br/orders/${orderId}/reject`, {
                 headers: {
                     accept: 'application/json',
                 },
